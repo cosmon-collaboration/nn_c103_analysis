596f369c73b3ead8f5f11ca9dfea9ce38383230a
walkloud <walksloud@gmail.com>
2023-10-02 15:36:44 -0700

    adding bs_utils to luscher

---
+++ luscher/bs_utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+import numpy as np
+import hashlib
+from numpy.random import Generator, SeedSequence, PCG64
+
+
+def get_rng(seed: str, verbose=False):
+    """Generate a random number generator based on a seed string."""
+    # Over python iteration the traditional hash was changed. So, here we fix it to md5
+    hash = hashlib.md5(seed.encode("utf-8")).hexdigest()  # Convert string to a hash
+    seed_int = int(hash, 16) % (10 ** 6)  # Convert hash to an fixed size integer
+    if verbose:
+        print("Seed to md5 hash:", seed, "->", hash, "->", seed_int)
+    # Create instance of random number generator explicitly to ensure long time support
+    # PCG64 -> https://www.pcg-random.org/
+    # see https://numpy.org/doc/stable/reference/random/generated/numpy.random.seed.html
+    rng = Generator(PCG64(SeedSequence(seed_int)))
+    return rng
+
+def make_bs_list(Ndata, Nbs, Mbs=None, seed=None, verbose=False, add_b0=True):
+    if Mbs:
+        m_bs = Mbs
+    else:
+        m_bs = Ndata
+
+    # seed the random number generator
+    rng = get_rng(seed,verbose=verbose) if seed else np.random.default_rng()
+
+    # make BS list: [low, high)
+    bs_list = rng.integers(low=0, high=Ndata, size=[Nbs, m_bs])
+
+    # addpend boot0?
+    if add_b0:
+        if m_bs != Ndata:
+            sys.exit('in order to insert boot0, Mbs must equal Ncfg: Ncfg=%d, Mbs=%d' %(NData,m_bs))
+        bs_list = np.insert(bs_list, 0, np.arange(Ndata), axis=0)
+
+    return bs_list
